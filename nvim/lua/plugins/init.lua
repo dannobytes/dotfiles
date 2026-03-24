e83be9c7d23ff1018ab5f39cec90fd356cdd2e76
Daniels Lee <dannlee@gmail.com>
2026-03-24 16:25:05 -0700

    chore: reformat

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,47 +1,47 @@
 return {
   {
-    'crusoexia/vim-monokai',
+    "crusoexia/vim-monokai",
     priority = 1000, -- High priority to load early
-    lazy = false,    -- Load immediately on startup
+    lazy = false, -- Load immediately on startup
     config = function()
-      vim.cmd('colorscheme monokai')
+      vim.cmd("colorscheme monokai")
     end,
   },
 
-  { 'tpope/vim-fugitive', }, -- Git integration
-  { 'tpope/vim-rhubarb', },  -- Enables :GBrowse
+  { "tpope/vim-fugitive" }, -- Git integration
+  { "tpope/vim-rhubarb" }, -- Enables :GBrowse
 
   -- UI and theming
-  { 'ap/vim-css-color' },            -- CSS color previews
-  { 'nvim-tree/nvim-web-devicons' }, -- Web dev icons
+  { "ap/vim-css-color" }, -- CSS color previews
+  { "nvim-tree/nvim-web-devicons" }, -- Web dev icons
 
   -- Editor functionality
   {
-    'editorconfig/editorconfig-vim',
+    "editorconfig/editorconfig-vim",
     config = function()
-      vim.g.EditorConfig_exclude_patterns = { 'fugitive://.*', 'scp://.*' }
-    end
+      vim.g.EditorConfig_exclude_patterns = { "fugitive://.*", "scp://.*" }
+    end,
   },
-  { 'tpope/vim-surround' },   -- Matching surround pairs
-  { 'tomtom/tcomment_vim' },  -- Easy comments
-  { 'skwp/vim-html-escape' }, -- HTML entity escaping
+  { "tpope/vim-surround" }, -- Matching surround pairs
+  { "tomtom/tcomment_vim" }, -- Easy comments
+  { "skwp/vim-html-escape" }, -- HTML entity escaping
 
   -- Language support
-  { 'sheerun/vim-polyglot' }, -- Syntax highlighting for many languages
-  { 'iloginow/vim-stylus' },  -- Stylus syntax
-  { 'moll/vim-node' },        -- Open files via ESM
+  { "sheerun/vim-polyglot" }, -- Syntax highlighting for many languages
+  { "iloginow/vim-stylus" }, -- Stylus syntax
+  { "moll/vim-node" }, -- Open files via ESM
 
   -- AI assistance
   {
-    'github/copilot.vim',
+    "github/copilot.vim",
     lazy = false,
     keys = {
       { "<c-l>", "<Plug>(copilot-accept-word)", mode = "i", desc = "Accept next Copilot suggestion word" },
-    }
+    },
   }, -- GitHub Copilot
 
   -- CodeCompanion dependencies
-  { 'echasnovski/mini.nvim' },
-  { 'nvim-treesitter/nvim-treesitter' },
-  { 'nvim-treesitter/nvim-treesitter-context' },
+  { "echasnovski/mini.nvim" },
+  { "nvim-treesitter/nvim-treesitter" },
+  { "nvim-treesitter/nvim-treesitter-context" },
 }
